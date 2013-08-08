0488b37c760ae2a959e2a3ecab5678b46382d340
jamesmccluskey <james.mccluskey@ntlworld.com>
2013-08-08 18:17:56 +0000

    Edited product desc in index.html

--- grader.js
+++ grader.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
+Automatically grade files for the presence of specific HTML tags/attributes.
+Uses commander.js and cheerio.  Teaches command line application development
 and basic DOM parsing.
 
 References:
@@ -30,9 +30,9 @@ var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    if(!fs.existsSync(instr)){
+        console.log("%s does not exist.  Exiting.", instr);
+        process.exit(1) // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -62,35 +62,31 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module) {
+if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL of page to check')
         .parse(process.argv);
     // If a url has been specified then check the page.  Otherwise check the file specified
-    if(program.url)
-    {
-        rest.get(program.url).on('complete', function(result)
-        { 
+    if(program.url) {
+        rest.get(program.url).on('complete', function(result) {        
             $ = cheerio.load(result);
             var checks = loadChecks(program.checks).sort();
             var out = {};
-
             for(var ii in checks) {
                 var present = $(checks[ii]).length > 0;
                 out[checks[ii]] = present;
             }
-
             var outJson = JSON.stringify(out, null, 4);
             console.log(outJson);
         });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
     }
-    else
-    {
-       var checkJson = checkHtmlFile(program.file, program.checks);
-       var outJson = JSON.stringify(checkJson, null, 4);
-       console.log(outJson);
-    }
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
